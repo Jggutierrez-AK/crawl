15bd6a092de57c4cc8d372b071f3b7507611bf9a
Jggutierrez-AK <ggutierrez@akurey.com>
2020-07-31 08:20:53 -0600

    delete dist folder and remake package.json

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,36 @@
+'use strict';
+
+var Fiber = require('fibers');
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var gulpIf = require('gulp-if');
-var cache = require('gulp-cache');
-var useref = require('gulp-useref');
-var cssnano = require('gulp-cssnano');
-var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync').create();
 
-var del = require('del');
-var runSequence = require('gulp4-run-sequence');
+sass.compiler = require('sass');
 
-gulp.task('browserSync', function() {
+gulp.task('browserSync', function(done) {
   browserSync.init({
     server: {
       baseDir: 'src'
     },
-  })
+    notify: false
+  });
+
+  browserSync.watch('src/index.html').on('change', browserSync.reload);
+  done()
 })
 
 gulp.task('sass', function(){
   return gulp.src('src/scss/style.scss')
-    .pipe(sass())
+  .pipe(sass({fiber: Fiber}).on('error', sass.logError))
     .pipe(gulp.dest('src/css'))
     .pipe(browserSync.reload({
       stream: true
     }))
 });
 
-gulp.task('useref', function(){
-  return gulp.src('src/index.html')
-    .pipe(useref())
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('dist'))
-});
-
-gulp.task('images', function(){
-  return gulp.src('src/assets/*.+(png|jpg|gif|svg)')
-  .pipe(cache(imagemin({
-    interlaced: true
-  })))
-  .pipe(gulp.dest('dist/assets'))
-});
-
-gulp.task('clean', function() {
-  return del('dist');
-});
-
-gulp.task('watch', gulp.parallel('sass', 'browserSync'), function(){
+gulp.task('watch', gulp.series('sass', 'browserSync', function(done) {
   gulp.watch('src/scss/style.scss', gulp.series('sass'));
-  gulp.watch('src/index.html', browserSync.reload); 
-});
 
-gulp.task('build', function (callback) {
-  runSequence('clean', 
-    ['sass', 'useref', 'images'],
-    callback
-  )
-});
+  done()
+}));
